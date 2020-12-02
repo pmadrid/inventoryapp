56e91ad4aa6c48c3996e351acd8ad57502a96b7e
pablomadridm <68798975+pablomadridm@users.noreply.github.com>
2020-12-01 19:02:59 -0500

    Primer commit

---
+++ toggle.js
@@ -0,0 +1,13 @@
+function toggle() {
+    let x = document.querySelectorAll("form");
+
+    for (i = 0; i < x.length; i++) {
+        if (x[i].classList.contains("invisible")) {
+            x[i].classList.remove("invisible");
+            x[i].classList.add("visible");
+        } else {
+            x[i].classList.remove("visible");
+            x[i].classList.add("invisible");
+        }
+    }
+}
